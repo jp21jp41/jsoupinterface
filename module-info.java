187f5ea1957d631ef5af5221418a5aeace06f842
Justin Pizano <102876431+jp21jp41@users.noreply.github.com>
2025-08-14 17:21:34 -0500

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module jsoupinterface {
+	exports jsoupinterface;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires javafx.fxml;
+	requires org.jsoup;
+}
